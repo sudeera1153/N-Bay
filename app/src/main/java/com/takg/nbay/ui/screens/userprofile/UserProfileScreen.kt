07e394e847bba22dab4d54a146e23e07c6fd1825
Sudeera Wijenayake <sudeera1153@gmail.com>
2022-05-16 10:24:52 +0530

    Added -userprofile

--- app/src/main/java/com/takg/nbay/ui/screens/userprofile/UserProfileScreen.kt
+++ app/src/main/java/com/takg/nbay/ui/screens/userprofile/UserProfileScreen.kt
@@ -1,9 +1,28 @@
 package com.takg.nbay.ui.screens.userprofile
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.takg.nbay.ui.screens.auth.signup.SignUpScreen
+import com.takg.nbay.ui.theme.NBayTheme
 
 @Composable
 fun UserProfileScreen()
 {
+    Column {
+        UserProfileAppBar()
+        UserProfileHead()
+        UserProfileContent()
+    }
 
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview2() {
+    NBayTheme {
+        UserProfileScreen()
+    }
 }
