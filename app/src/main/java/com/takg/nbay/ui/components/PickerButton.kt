c503f769224df5a99a3c133a95315023bfea451a
Sudeera Wijenayake <sudeera1153@gmail.com>
2022-05-17 06:29:53 +0530

    Add Payment Proceeding Screen

---
+++ app/src/main/java/com/takg/nbay/ui/components/PickerButton.kt
@@ -0,0 +1,15 @@
+/*
+package com.takg.nbay.ui.components
+
+import android.widget.Toast
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HorizontalNumberPicker(
+    min = 10,
+    max = 100,
+    default = 50,
+    onValueChange = { value ->
+        Toast.makeText(context, value.toString(), Toast.LENGTH_SHORT).show()
+    }{}
+)*/
